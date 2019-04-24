9b79b8e154920e02aebde9bd81f00c84da4b2643
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-04-23 19:02:39 -0600

    Super mega cambio radical XD

--- app/src/main/java/com/naldana/ejemplo11/MainActivity.kt
+++ app/src/main/java/com/naldana/ejemplo11/MainActivity.kt
@@ -1,109 +1,107 @@
 package com.naldana.ejemplo11
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
 import java.lang.StringBuilder
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private val filepath = "MyFileStorage"
-    internal var myExternalFile: File?=null
-    val filename = "email.txt"
-    val fileNameDos = "email0.txt"
+    companion object {
+        const val REQUEST_PERMISSION = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO (1) Obtener acceso a Shared Preference
-        // TODO (2) Obtener Shared Preference desde la actividad
-        // TODO (3) Especificar el nombre del archivo de preferencia (si se esta usando mas de uno)
-        // TODO (3.1) Cuando se necesita solamente un archivo de preferencia en el app llamar a getPreferences con un paramentro
-        // TODO (4) Asignar que la preferencias se leeran o escribiran en modo privado (Para mantender privado las configuración)
-        val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
 
+        val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+        var correo = sharedPref.getString(getString(R.string.save_email_key), "")
+        tv_data.text = correo
 
         bt_save.setOnClickListener {
-            // TODO (5) Para escribir valores en shared preference
+
             with(sharedPref.edit()) {
-                putString(getString(R.string.save_email_key), et_option.text.toString()) // TODO (6) Se guardan en formato clave valor
-                commit() // TODO (7) Confirma que se guarden todos los elementos añadidos
+                putString(getString(R.string.save_email_key), et_option.text.toString())
+                commit()
 
             }
+
 
             tv_data.text = et_option.text.toString() // Solamente para mostrar el valor de inmediato
         }
 
-
-        // TODO (8) Para leer basta con ejecutar el metodo getXXXX y definir el valor por defecto si no existe
-        val email = sharedPref.getString(getString(R.string.save_email_key), "")
-
-        tv_data.text = email
-
-
-        bt_write_internal.setOnClickListener {
-            // TODO (9) openFileOutput crea un archivo y un InputStream para escribir en el
-
-            val fileContent = "email: $email"
-
-            // TODO (10) Al usar use obtenemos el fileInput que devuelve FileOutputStream
-            // TODO (11) use cierra el FileOutputStream y maneja la exception a nivel de bloque
-
-            openFileOutput(filename,Context.MODE_PRIVATE).use {
-                it.write(fileContent.toByteArray())
+        bt_write_external.setOnClickListener{
+            correo = sharedPref.getString(getString(R.string.save_email_key), "")
+            val nombreArchivo= "ok.txt"
+            try{
+                var memoriasd= Environment.getExternalStorageDirectory()
+                var rutaSD= File(memoriasd.path, nombreArchivo);
+                var crear = OutputStreamWriter(FileOutputStream(rutaSD));
+                crear.write(correo);
+                crear.flush();
+                crear.close();
+            }catch(e: FileNotFoundException){
+                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
             }
-            Toast.makeText(applicationContext,"data save",Toast.LENGTH_SHORT).show()
         }
 
 
-        bt_write_external.setOnClickListener {
-            //val fileNameDos = "email0.txt"
-            myExternalFile=File(getExternalFilesDir(filepath),fileNameDos)
-            try {
-                val fileOutputStream = FileOutputStream(myExternalFile)
-                fileOutputStream.write(et_option.text.toString().toByteArray())
-                fileOutputStream.close()
-            }catch (e:IOException){
-                e.printStackTrace()
+
+        bt_write_internal.setOnClickListener {
+            correo = sharedPref.getString(getString(R.string.save_email_key), "")
+            val nombreArchivo = "email.txt"
+            val fileContent = "email: $correo"
+
+
+            openFileOutput(nombreArchivo,Context.MODE_PRIVATE).use {
+                it.write(fileContent.toByteArray())
             }
+
         }
 
 
         bt_read_internal.setOnClickListener{
-            // TODO (12) Abrir un archivo existente
-            //val filenames = "email.txt"
-            openFileInput(filename).use {
-                val text = it.bufferedReader().readText() // TODO (13) Se lee todo el contenido
+
+            val nombreArchivo = "email.txt"
+            openFileInput(nombreArchivo).use {
+                val text = it.bufferedReader().readText()
                 tv_data.text = text
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION)
+        } else {
+            montarRuta()
+        }
+    }
 
-
-        bt_read_external.setOnClickListener {
-            //
-            myExternalFile=File(getExternalFilesDir(filepath),fileNameDos)
-
-            val filnombre = fileNameDos
-
-            myExternalFile=File(getExternalFilesDir(filepath),filnombre)
-
-            if(filnombre.toString()!=null && filnombre.toString().trim()!=""){
-                var fileInputStream = FileInputStream(myExternalFile)
-                var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
-                var bufferedReader:BufferedReader= BufferedReader(inputStreamReader)
-                val stringBuilder:StringBuilder= StringBuilder()
-                var text:String?=null
-                while({text=bufferedReader.readLine();text}()!=null){
-                    stringBuilder.append(text)
-                }
-                fileInputStream.close()
-                Toast.makeText(applicationContext,stringBuilder.toString(),Toast.LENGTH_SHORT).show()
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                montarRuta()
             }
         }
     }
 
-
+    private fun montarRuta() {
+        val direccion = "${Environment.getExternalStorageDirectory()}/$packageName"
+        File(direccion).mkdirs()
+        val file = "%1\$tY%1\$tm%1\$td%1\$tH%1\$tM%1\$tS.log".format(Date())
+        File("$direccion/$file").printWriter().use {
+            it.println("text")
+        }
+    }
 }
